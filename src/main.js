3c51afdf84278be41d1db68d00239c8385f93298
Carlos Matheu Armengol <info@plastikaweb.com>
2017-09-16 20:56:20 +0200

    check filter component

--- src/main.js
+++ src/main.js
@@ -3,39 +3,48 @@ import './style.scss';
 import genres from './util/genres';
 
 new Vue({
-  el: '#app',
-  components: {
-    'movie-list': {
-      template: `<div id="movie-list">
+    el: '#app',
+    components: {
+        'movie-list': {
+            template: `<div id="movie-list">
                         <div v-for="movie in movies" class="movie">{{movie.title}}</div>
                          </div>`,
-      data: function() {
-        return {
-          movies: [
-            { title: 'Pulp fiction' },
-            { title: 'Home alone' },
-            { title: 'Austin Powers' }
-          ]
-        };
-      }
-    },
-    'movie-filter': {
-      data() {
-        return {
-          genres
-        };
-      },
-      template: `<div id="movie-filter">
+            data: function () {
+                return {
+                    movies: [
+                        {title: 'Pulp fiction'},
+                        {title: 'Home alone'},
+                        {title: 'Austin Powers'}
+                    ]
+                };
+            }
+        },
+        'movie-filter': {
+            data() {
+                return {
+                    genres
+                };
+            },
+            template: `<div id="movie-filter">
                <h2>Filter results</h2>
                <div class="filter-group">
-               <check-filter v-for="genre in genres">{{genre}}</check-filter>
+               <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
                </div>
             </div>`,
-      components: {
-        'check-filter': {
-          template: `<div>Filter</div>`
+            components: {
+                'check-filter': {
+                    data() {
+                        return {
+                            checked: false
+                        };
+                    },
+                    props: ['title'],
+                    template: `<span v-bind:class="{'check-filter': true, active: checked}" class="check-filter" v-on:click="checked = !checked">
+                                    <span class="checkbox"></span>
+                                    <span class="check-filter-title">{{title}}</span>
+                                </div>`
+                }
+            }
         }
-      }
     }
-  }
 });
