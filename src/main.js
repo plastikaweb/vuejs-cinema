89dec0931c4df2af8eb4f8005e979f50598077a1
Carlos Matheu Armengol <info@plastikaweb.com>
2017-09-17 19:27:17 +0200

    added filter movies by genre behavior

--- src/main.js
+++ src/main.js
@@ -4,19 +4,49 @@ import genres from './util/genres';
 
 new Vue({
     el: '#app',
+    data: {
+        genre: [],
+        time: []
+    },
+    methods: {
+        checkFilter(category, title, checked) {
+            if (checked) {
+                this[category].push(title);
+            } else {
+                const index = this[category].indexOf(title);
+                if (index > -1) {
+                    this[category].splice(index, 1);
+                }
+            }
+        }
+    },
     components: {
         'movie-list': {
             template: `<div id="movie-list">
-                        <div v-for="movie in movies" class="movie">{{movie.title}}</div>
+                        <div v-for="movie in filteredMovies" class="movie">{{movie.title}}</div>
                          </div>`,
             data: function () {
                 return {
                     movies: [
-                        {title: 'Pulp fiction'},
-                        {title: 'Home alone'},
-                        {title: 'Austin Powers'}
+                        {title: 'Pulp fiction', genre: genres.CRIME},
+                        {title: 'Home alone', genre: genres.COMEDY},
+                        {title: 'Austin Powers', genre: genres.COMEDY}
                     ]
                 };
+            },
+            props: ['genre', 'time'],
+            methods: {
+                moviePassesGenreFilter(movie) {
+                    if(!this.genre.length) {
+                        return true;
+                    }
+                    return this.genre.find(genre => movie.genre === genre);
+                }
+            },
+            computed: {
+                filteredMovies() {
+                    return this.movies.filter(this.moviePassesGenreFilter);
+                }
             }
         },
         'movie-filter': {
@@ -28,9 +58,14 @@ new Vue({
             template: `<div id="movie-filter">
                <h2>Filter results</h2>
                <div class="filter-group">
-               <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
+               <check-filter v-for="genre in genres" v-on:check-filter="checkFilter" v-bind:title="genre"></check-filter>
                </div>
             </div>`,
+            methods: {
+                checkFilter(category, title, checked) {
+                    this.$emit('check-filter', category, title, checked);
+                }
+            },
             components: {
                 'check-filter': {
                     data() {
@@ -39,10 +74,16 @@ new Vue({
                         };
                     },
                     props: ['title'],
-                    template: `<span v-bind:class="{'check-filter': true, active: checked}" class="check-filter" v-on:click="checked = !checked">
+                    template: `<span v-bind:class="{'check-filter': true, active: checked}" class="check-filter" v-on:click="checkFilter">
                                     <span class="checkbox"></span>
                                     <span class="check-filter-title">{{title}}</span>
-                                </div>`
+                                </div>`,
+                    methods: {
+                        checkFilter() {
+                            this.checked = !this.checked;
+                            this.$emit('check-filter', 'genre', this.title, this.checked);
+                        }
+                    }
                 }
             }
         }
